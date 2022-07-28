7be5c92112a48d946f0a91e9eeee70ee2b2e0768
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2022-07-28 19:29:34 +0000

    Quarta Aula

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../components/Logo";
 export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="mac-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
           <Logo />
           
@@ -15,9 +15,30 @@ export function Subscribe() {
           </p>
         </div>
 
-        <div>
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+          
+          <form action="" className="flex flex-col gap-2 w-full">
+            <input 
+              className="bg-gray-900 rounded px-5 h-15 h-14"
+              type="text"
+              placeholder="Seu nome completo"
+            />
+            <input 
+              className="bg-gray-900 rounded px-5 h-15 h-14"
+              type="text"
+              placeholder="Seu nome completo"
+            />
 
+            <button
+              type="submit"
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors duration-200 ease-in-out"
+            >
+              Garantir a minha vaga
+            </button>
+          </form>
         </div>
+
       </div>
       
       <img src="/src/assets/code-mockup.png" className="mt-10"/>
