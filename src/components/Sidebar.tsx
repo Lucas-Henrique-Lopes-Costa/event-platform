16f3e51922f9cbccb25dc4c6b154f2497ac2bd8e
Lucas <lucass20586@gmail.com>
2022-07-30 14:58:47 -0300

    create codegen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,9 @@
 import { gql, useQuery } from "@apollo/client";
+import { useGetLessonQuery } from "../generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-  query MyQuery {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      availableAt
-      title
-      slug
-    }
-  }
-`
-
-// Tipo do Type
-interface GetLessonsQueryResponse {
-  lessons: {
-    id: string
-    title: string
-    slug: string
-    availableAt: string
-    lessonType: 'live' | 'class'
-  }[]
-}
-
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+  const { data } = useGetLessonQuery()
   
   return(
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
